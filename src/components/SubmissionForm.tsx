8a5496a58a4056399bb1876f8e8d171c6f4bf85d
Tristan Varewijck <tristan.varewijck@gmail.com>
2025-08-13 18:25:13 +0200

    start up script for production

--- src/components/SubmissionForm.tsx
+++ src/components/SubmissionForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
